d8f454e84f0601a219bba2b416ff2c28a7828d52
dragan <dragang@iborn.net>
2019-12-02 21:08:04 +0100

    class 1 finito

--- g7/class1/script.js
+++ g7/class1/script.js
@@ -31,9 +31,9 @@ let isCool = true;
 console.log(isCool);
 
 // Valid names
-
-let name = "Bob";
+// YOU CANT HAVE 2 VARIABLES WITH THE SAME NAME
 let name1 = "Bob";
+let name2 = "Bob";
 let $name = "Bob";
 let _name = "Bob";
 let $2name = "Bob";
@@ -46,3 +46,30 @@ let $2name = "Bob";
 // let #name = "Bob";
 // let = "Bob";
 
+// Operators
+// JAVASCRIPT ALWAYS EXECUTE EXPRESSION FIRST
+2 + 3; // Expression
+let result1 = 2 + 3;
+// 1. Declares space for result1
+// 2. Executes expression 2 + 3 
+// 3. Adds the result 5 in to result1 variable
+console.log(result1);
+console.log(2 + 5);
+// 1. Write something in the console
+// 2. Execute the expression 2 + 5
+// 3. Put the result 7 in the console.log()
+// 4. Execute the console.log with the result 7
+let comparison = 3 > 5;
+console.log(comparison);
+let comparison2 = result1 <= 5; // 5 <= 5
+console.log(comparison2);
+
+// Equals ?
+// = - Assign avariable or assign value
+// == - Are two things equals of value
+// === - Are two things equal of value AND TYPE
+console.log(2 == "2");
+console.log(2 === "2");
+console.log((2 > 3) != 4);
+// 1. 2 > 3  ->  false
+// 2. false != 4  ->  true
