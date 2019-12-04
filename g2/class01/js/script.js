7d3e16f6cd491d9579895980f1394efaecdd9f97
Martin Panovski <martinp@iborn.net>
2019-12-04 22:42:23 +0100

    Class01 added

---
+++ g2/class01/js/script.js
@@ -0,0 +1,118 @@
+// console.log("Test!");
+
+//************* VARIABLES - DECLARATION AND INITIALIZATION **************************
+
+let randomNumber;
+randomNumber = 15;
+
+let name = "Dejan";
+let secondName = "Martin";
+
+let num1 = 5;
+let num2 = 10;
+
+let isRaining = false;
+
+let notDefined;
+let nullValue = null;
+
+// console.log("randomNumber"); // this will be printed as a string, not as a variable value!
+
+/* Multiple line comment
+console.log(randomNumber);
+console.log(name);
+console.log(num1);
+console.log(isRaining);
+console.log(notDefined);
+console.log(nullValue);
+*/
+
+//************* VARIABLE TYPES  **************************
+// console.log(typeof randomNumber);
+// console.log(typeof name);
+// console.log(typeof isRaining);
+// console.log(typeof notDefined);
+// console.log(typeof nullValue);  
+
+//**** Good practice! ****
+// let a = 5;
+// let b = 6;
+
+// let sum = a + b;
+// console.log(sum);
+
+//**** Bad practice! ****
+// console.log(5 + 6);
+
+//************* Cannot declare variables by using KEYWORDS! **************************
+// let break = 6;
+
+
+//************* ARITHMETIC OPERATORS **************************
+
+// let number1 = 50;
+// let number2 = 25;
+// let number3 = 25.5;
+
+// let addition = number1 + number2;
+// console.log("Addition: ", addition);
+
+// let subtraction = number1 - number3;
+// console.log("Subtraction: ", subtraction);
+
+// let multiplication = number2 * number3;
+// console.log("Multiplication: ", multiplication);
+
+// let division = number1 / number3;
+// console.log("Division: ", division);
+
+// let reminder1 = number1 % number2;
+// console.log("Reminder 1: ",reminder1);
+
+// let reminder2 = number1 % number3;
+// console.log("Reminder 2: ",reminder2);
+
+// let numberIncremented = ++number1;
+// console.log("Incrementation: ", numberIncremented);
+
+// let numberDecremented = number1--;
+// console.log("Decrementation: ", numberDecremented);
+
+
+//BONUS
+// let c = 10;
+// let temp = c;
+// let a = temp;
+// console.log(a);
+
+//************* ASSIGNMENT OPERATORS **************************
+
+// let x = 10;
+// let y = 0;
+// y += x;
+// console.log(y);
+
+
+//************* Class exercises **************************
+//First exercise
+
+// let feet = 100;
+// let conversionRate = 0.3048;
+// let feetToMeters = feet * conversionRate;
+// console.log("100 feet converted in meters is: ", feetToMeters);
+
+//Second exercise
+
+// let sideA = 10;
+// let sideB = 15.5;
+// let rectangleArea = sideA * sideB;
+// console.log("The area of the rectangle is: ", rectangleArea);
+
+//Third exercise
+
+// let radius = 10;
+// let pi = 3.14;
+// let circleArea = radius * radius * pi;
+// console.log("The area of the circle is: ", circleArea);
+
+
