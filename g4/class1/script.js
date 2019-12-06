7d87051090d16217e8f7c44b1a15bd35ca9215e2
viktorjakovlev <viktorjakovlev@hotmail.com>
2019-12-06 18:05:18 +0100

    class1

---
+++ g4/class1/script.js
@@ -0,0 +1,49 @@
+//alert in to the brawser
+alert("Hello from external JS document!");
+
+//log result in to the ocnsole
+console.log("Hello from external JS document!");
+
+//variables
+let text = "Text";
+let number = 10;
+let bool = true; //false
+
+//decaration
+let name;
+
+//initialization
+name = "Kristina";
+
+//decaration and initialization
+let anotherName = "Viktor";
+
+//Write an JavaScript program to convert a 
+//length given in feet to meters
+
+let feet = 100;
+let feetToMeters = feet * 0.3048;
+
+console.log(feet + " feet(s) are equal to " + feetToMeters + " meters.");
+
+//Write an JavaScript program that will read the two 
+//sides of a rectangle and calculate its area
+
+let sideA = 5;
+let sideB = 10;
+let rectangleArea = sideA * sideB;
+
+console.log("Rectangle area is equal to " + rectangleArea + " m2.");
+
+//Write an JavaScript program that will 
+//calculate area of Circle
+
+let radius = 10; 
+const Pi = Math.Pi;
+let circleArea = Pi * (radius * radius);
+
+console.log("The area of the circle with the radius of " + radius + " m is " + circleArea + " m2.");
+
+
+// online text editor
+// https://playcode.io/  :)
