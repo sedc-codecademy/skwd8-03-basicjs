65e4670c4d47d0a59562a64f48a7b7519a5c2731
viktorjakovlev <viktorjakovlev@hotmail.com>
2019-12-14 14:50:37 +0100

    updates

--- g4/exercise2/script.js
+++ g4/exercise2/script.js
@@ -2,20 +2,42 @@ let balance = 1500.0;
 
 function getBalace() {
     alert(`Your current balance is ${balance}.`);
+    atm();
 }
 
 function makeDeposit(deposit) {
     if(isNaN(deposit) || deposit === "") {
         alert("Error: please enter a valid amount of money!")
+        atm();
     } else {
         balance += deposit;
         getBalace();
     }
 }
 
+function makeWithdrawal(withdraw) {
+    if(isNaN(withdraw) || withdraw === "") {
+        alert("Error: please enter a valid amount of money!")
+        atm();
+    } else {
+        balance -= withdraw;
+        getBalace();
+    }
+ }
 
+ function exit() {
+     let confirm = window.confirm("yes / no");
 
+     if(confirm) {
+        window.close();
+     } else {
+        atm();
+     }
+ } 
 
+ function error() {
+     alert("Wrong input!");
+ }
 
 function atm() {
     let choice = parseInt(prompt("Select a choice: 1). Balance 2). Deposit 3). Withdrawal 4). Exit"))
@@ -26,11 +48,13 @@ function atm() {
         let deposit = parseFloat(prompt("How much money would you like to deposit?"))
         makeDeposit(deposit);
     } else if (choice === 3) {
-        
-        makeWithdrawal()
+        let withdraw = parseFloat(prompt("How much money would you like to withdraw?"))
+        makeWithdrawal(withdraw)
     } else if (choice === 4) {
         exit();
     } else {
         error();
     }
 }  
+
+atm();
